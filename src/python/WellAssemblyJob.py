0f3318edd21f62434fc036ffbc516cedd32f01f9
Raymond LeClair <raymond.leclair@addgene.org>
2018-12-14 17:08:54 -0500

    Added main method to assembly class to run the job.

--- src/python/WellAssemblyJob.py
+++ src/python/WellAssemblyJob.py
@@ -1,10 +1,16 @@
+from argparse import ArgumentParser
+import os
+
 from toil.job import Job
+from toil.common import Toil
 
 from ApcJob import ApcJob
 from SpadesJob import SpadesJob
 
+import utilities
 
-class AssemblyJob(Job):
+
+class WellAssemblyJob(Job):
     """
     Runs a SpadesJob followed by an ApcJob
     """
@@ -24,7 +30,7 @@ class AssemblyJob(Job):
         output_directory : str
             name of directory for output
         """
-        super(AssemblyJob, self).__init__(*args, **kwargs)
+        super(WellAssemblyJob, self).__init__(*args, **kwargs)
         self.read_one_file_id = read_one_file_id
         self.read_two_file_id = read_two_file_id
         self.coverage_cutoff = coverage_cutoff
@@ -53,3 +59,60 @@ class AssemblyJob(Job):
 
         # Return file ids for export
         return final_job.rv()
+
+
+if __name__ == "__main__":
+    """
+    Assemble reads corresponding to a single well.
+    """
+
+    # Parese data directory, plate and well specification, and
+    # coverage cutoff
+    parser = ArgumentParser()
+    Job.Runner.addToilOptions(parser)
+    parser.add_argument('-d', '--data-directory',
+                        default="../../dat/miscellaneous",
+                        help="the plate specification")
+    parser.add_argument('-p', '--plate-spec', default="A11967A_sW0154",
+                        help="the plate specification")
+    parser.add_argument('-w', '--well-spec', default="A01",
+                        help="the well specification")
+    parser.add_argument('-c', '--coverage-cutoff', default="100",
+                        help="the coverage cutoff")
+    options = parser.parse_args()
+
+    # Work within the Toil context manager
+    with Toil(options) as toil:
+        if not toil.options.restart:
+
+            # Import the local read files into the file store
+            read_one_file_id = utilities.importFile(
+                toil, os.path.join("../../dat/miscellaneous",
+                                   "{0}_FASTQ/{0}_{1}_R1_001.fastq.gz".format(
+                                       options.plate_spec, options.well_spec)))
+            read_two_file_id = utilities.importFile(
+                toil, os.path.join("../../dat/miscellaneous",
+                                   "{0}_FASTQ/{0}_{1}_R2_001.fastq.gz".format(
+                                       options.plate_spec, options.well_spec)))
+
+            # Construct and start the well assembly job
+            assembly_job = WellAssemblyJob(
+                read_one_file_id,
+                read_two_file_id,
+                options.coverage_cutoff,
+                options.plate_spec,
+                )
+            assembly_rv = toil.start(assembly_job)
+
+        else:
+
+            # Restart the well assembly job
+            assembly_rv = toil.restart(assembly_job)
+
+        # Export the SPAdes warnings and log files, and contigs FASTA
+        # file from the file store
+        utilities.exportFiles(toil, assembly_rv['spades_rv'])
+
+        # Export the apc output file, and sequence FASTA file from the
+        # file store
+        utilities.exportFiles(toil, assembly_rv['apc_rv'])
