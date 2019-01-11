ba7243ea08da3f6051186a0261c018391048f9ae
Raymond LeClair <raymond.leclair@addgene.org>
2019-01-11 13:05:41 -0500

    Collected common methods to a Toil test class. Completed well and plate test cases.

--- src/python/JobsTest.py
+++ src/python/JobsTest.py
@@ -1,5 +1,6 @@
 import filecmp
 import os
+import shutil
 import unittest
 
 from toil.job import Job
@@ -7,11 +8,68 @@ from toil.common import Toil
 
 from ApcJob import ApcJob
 from SpadesJob import SpadesJob
+from WellAssemblyJob import WellAssemblyJob
+from PlateAssemblyJob import PlateAssemblyJob
 
 import utilities
 
 
-class JobsTestCase(unittest.TestCase):
+class ToilTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.data_directory = os.path.join("..", "..", "dat", "miscellaneous")
+
+        self.plate_spec = "A11967A_sW0154"
+        self.well_spec = "A01"
+        self.coverage_cutoff = "100"
+
+        self.output_directory = "{0}_{1}".format(self.plate_spec, self.well_spec)
+        self.actual_directory_a = self.output_directory
+        self.test_directory_a = self.output_directory + "_TEST"
+        if not os.path.exists(self.test_directory_a):
+            os.mkdir(self.test_directory_a)
+
+        self.well_specs = ["G05", "G06"]
+
+        self.actual_directory_b = "A11967B_sW0154"
+        self.test_directory_b = self.actual_directory_b + "_TEST"
+        if not os.path.exists(self.test_directory_b):
+            os.mkdir(self.test_directory_b)
+
+        self.spades_fasta = "contigs.fasta"
+        self.apc_fasta = "apc.1.fa"
+
+    def tearDown(self):
+        if os.path.exists(self.test_directory_a):
+            shutil.rmtree(self.test_directory_a)
+        if os.path.exists(self.test_directory_b):
+            shutil.rmtree(self.test_directory_b)
+
+    def _import_read_files(self, toil, well_specs):
+        read_one_file_ids, read_two_file_ids = utilities.importReadFiles(
+            toil, self.data_directory, self.plate_spec, well_specs)
+        return read_one_file_ids, read_two_file_ids
+
+    def _import_contigs_file(self, toil):
+        contigs_file_id = utilities.importFile(
+            toil, os.path.join("{0}_{1}".format(
+                self.plate_spec, self.well_spec), self.spades_fasta))
+        return contigs_file_id
+
+    def _assert_true_cmp_spades_fasta(self, actual_directory):
+        self.assertTrue(
+            filecmp.cmp(
+                os.path.join(self.test_directory_a, self.spades_fasta),
+                os.path.join(actual_directory, self.spades_fasta)))
+
+    def _assert_true_cmp_apc_fasta(self, actual_directory):
+        self.assertTrue(
+            filecmp.cmp(
+                os.path.join(self.test_directory_a, self.apc_fasta),
+                os.path.join(actual_directory, self.apc_fasta)))
+
+
+class JobsTestCase(ToilTestCase):
 
     def test_spades_job(self):
 
@@ -19,38 +77,24 @@ class JobsTestCase(unittest.TestCase):
 
         with Toil(options) as toil:
 
-            read_one_file_id = utilities.importFile(
-                toil, os.path.join(
-                    "..", "..", "dat", "miscellaneous", "A11967A_sW0154_FASTQ",
-                    "A11967A_sW0154_A01_R1_001.fastq.gz"
-                )
-            )
-            read_two_file_id = utilities.importFile(
-                toil, os.path.join(
-                    "..", "..", "dat", "miscellaneous", "A11967A_sW0154_FASTQ",
-                    "A11967A_sW0154_A01_R2_001.fastq.gz"
-                )
-            )
-
-            coverage_cutoff = "100"
-            output_directory = "A11967A_sW0154_A01"
+            read_one_file_ids, read_two_file_ids = self._import_read_files(
+                toil, [self.well_spec])
 
             spades_job = SpadesJob(
-                read_one_file_id,
-                read_two_file_id,
-                coverage_cutoff,
-                output_directory,
+                read_one_file_ids[0],
+                read_two_file_ids[0],
+                self.coverage_cutoff,
+                self.output_directory,
             )
             spades_rv = toil.start(spades_job)
 
-            utilities.exportFiles(toil, spades_rv['spades_rv'])
+            utilities.exportFiles(
+                toil, self.test_directory_a, spades_rv['spades_rv'])
 
         self.assertTrue(
             filecmp.cmp(
-                os.path.join("A11967A_sW0154_A01", "contigs.fasta"),
-                "contigs.fasta"
-            )
-        )
+                os.path.join(self.test_directory_a, self.spades_fasta),
+                os.path.join(self.actual_directory_a, self.spades_fasta)))
 
     def test_apc_job(self):
 
@@ -58,44 +102,89 @@ class JobsTestCase(unittest.TestCase):
 
         with Toil(options) as toil:
 
-            contigs_file_id = utilities.importFile(
-                toil, os.path.join("A11967A_sW0154_A01", "contigs.fasta"))
+            contigs_file_id = self._import_contigs_file(toil)
 
             apc_job = ApcJob(contigs_file_id)
             apc_rv = toil.start(apc_job)
 
-            utilities.exportFiles(toil, apc_rv['apc_rv'])
+            utilities.exportFiles(
+                toil, self.test_directory_a, apc_rv['apc_rv'])
 
         self.assertTrue(
             filecmp.cmp(
-                os.path.join("A11967A_sW0154_A01", "apc.1.fa"),
-                "apc.1.fa"
-            )
-        )
+                os.path.join(self.test_directory_a, self.apc_fasta),
+                os.path.join(self.actual_directory_a, self.apc_fasta)))
 
 
-class WellJobTestCase(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
+class WellAssemblyJobTestCase(ToilTestCase):
 
     def test_well_assembly_job(self):
-        pass
+
+        options = Job.Runner.getDefaultOptions("wellAssemblyJobStore")
+
+        with Toil(options) as toil:
+
+            read_one_file_ids, read_two_file_ids = self._import_read_files(
+                toil, [self.well_spec])
+
+            well_assembly_job = WellAssemblyJob(
+                read_one_file_ids[0],
+                read_two_file_ids[0],
+                self.coverage_cutoff,
+                self.output_directory,
+            )
+            well_assembly_rv = toil.start(well_assembly_job)
+
+            utilities.exportFiles(
+                toil, self.test_directory_a, well_assembly_rv['spades_rv'])
+            utilities.exportFiles(
+                toil, self.test_directory_a, well_assembly_rv['apc_rv'])
+
+        self.assertTrue(
+            filecmp.cmp(
+                os.path.join(self.test_directory_a, self.spades_fasta),
+                os.path.join(self.actual_directory_a, self.spades_fasta)))
+        self.assertTrue(
+            filecmp.cmp(
+                os.path.join(self.test_directory_a, self.apc_fasta),
+                os.path.join(self.actual_directory_a, self.apc_fasta)))
 
 
-class PlateJobTestCase(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
+class PlateJobTestCase(ToilTestCase):
 
     def test_plate_assembly_job(self):
-        pass
+
+        options = Job.Runner.getDefaultOptions("plateAssemblyJobStore")
+
+        with Toil(options) as toil:
+
+            read_one_file_ids, read_two_file_ids = self._import_read_files(
+                toil, self.well_specs)
+
+            plate_assembly_job = PlateAssemblyJob(
+                self.well_specs,
+                read_one_file_ids,
+                read_two_file_ids,
+                self.plate_spec,
+                self.coverage_cutoff,
+                cores=2,
+                disk="3G",
+                memory="4G",
+                )
+            well_assembly_rvs = toil.start(plate_assembly_job)
+
+            utilities.exportWellAssemblyFiles(
+                toil, self.test_directory_b, self.well_specs, well_assembly_rvs)
+
+        for well_spec in self.well_specs:
+            self.assertTrue(
+                filecmp.cmp(
+                    os.path.join(self.test_directory_b, well_spec, self.spades_fasta),
+                    os.path.join(self.actual_directory_b, well_spec, self.spades_fasta)))
+            self.assertTrue(
+                filecmp.cmp(
+                    os.path.join(self.test_directory_b, well_spec, self.apc_fasta),
+                    os.path.join(self.actual_directory_b, well_spec, self.apc_fasta)))
 
 
 if __name__ == '__main__':
@@ -105,7 +194,7 @@ if __name__ == '__main__':
     jobsTestSuite.addTest(JobsTestCase('test_apc_job'))
 
     wellJobTestSuite = unittest.TestLoader().loadTestsFromTestCase(
-        WellJobTestCase)
+        WellAssemblyJobTestCase)
 
     plateJobTestSuite = unittest.TestLoader().loadTestsFromTestCase(
         PlateJobTestCase)
@@ -117,4 +206,3 @@ if __name__ == '__main__':
     ])
 
     unittest.TextTestRunner(verbosity=2).run(testSuites)
-    
