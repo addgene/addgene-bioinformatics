a6c58db211a13d326bc00dbb52b9ec28e473540d
Raymond LeClair <raymond.leclair@addgene.org>
2019-10-13 19:03:44 -0400

    Added main method to run APC independently.

--- src/python/ApcJob.py
+++ src/python/ApcJob.py
@@ -1,6 +1,9 @@
+from argparse import ArgumentParser
+import os
 import subprocess
 
 from toil.job import Job
+from toil.common import Toil
 
 import utilities
 
@@ -75,3 +78,51 @@ class ApcJob(Job):
         }
         apc_rv.update(self.parent_rv)
         return apc_rv
+
+
+
+if __name__ == "__main__":
+    """
+    Circularize contigs corresponding to a single well.
+
+    """
+    # Parse FASTA data directory and file name, and output directory,
+    # making the output directory if needed
+    parser = ArgumentParser()
+    Job.Runner.addToilOptions(parser)
+    parser.add_argument('-d', '--data-directory',
+                        default=os.path.join("..", "..", "dat", "miscellaneous", "A11967A_sW0154_FASTA"),
+                        help="the directory containing FASTA contigs files")
+    parser.add_argument('-f', '--file-name', default="A01-A11967A-contigs.fasta",
+                        help="the FASTA contigs file")
+    parser.add_argument('-o', '--output-directory', default=None,
+                        help="the directory containing all output files")
+    options = parser.parse_args()
+    if options.output_directory is None:
+        options.output_directory = options.data_directory
+    if not os.path.exists(options.output_directory):
+        os.mkdir(options.output_directory)
+
+    # Work within the Toil context manager
+    with Toil(options) as toil:
+        if not toil.options.restart:
+
+            # Import the local contigs file into the file store
+            contigs_file_id = utilities.importContigsFile(
+                toil, options.data_directory, options.file_name)
+
+            # Construct and start the APC job
+            apc_job = ApcJob(
+                contigs_file_id,
+                )
+            apc_rv = toil.start(apc_job)
+
+        else:
+
+            # Restart the APC job
+            apc_rv = toil.restart(apc_job)
+
+        # Export the apc output file, and sequence FASTA file from the
+        # file store
+        utilities.exportFiles(
+            toil, options.output_directory, apc_rv['apc_rv'])
