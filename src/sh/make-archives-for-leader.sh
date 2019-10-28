53ee81f0b134e2c1e6e683d8139de74777cf71c7
Raymond LeClair <raymond.leclair@addgene.org>
2019-10-28 12:17:21 -0400

    Removed an unneeded script, and added one needed script.

---
+++ src/sh/make-archives-for-leader.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd | sed s%/src/sh%% )"
+
+pushd $HOME/src/python
+tar -czvf python.tar.gz A11967* *.py
+mv python.tar.gz $HOME
+popd
+
+pushd $HOME/dat
+tar -czvf miscellaneous.tar.gz \
+    miscellaneous/A11967A_sW0154_FASTQ/A11967A_sW0154_A* \
+    miscellaneous/A11967A_sW0154_FASTQ/A11967A_sW0154_B01_* \
+    miscellaneous/A11967A_sW0154_FASTQ/A11967A_sW0154_G05_* \
+    miscellaneous/A11967A_sW0154_FASTQ/A11967A_sW0154_G06_* \
+    miscellaneous/A11967B_sW0154_FASTQ
+mv miscellaneous.tar.gz $HOME
+popd
