d228168e07d53c4dd129e4fc7fa376772b4b2dc6
Benjamin Lee <benjamindlee@me.com>
2019-10-29 21:04:54 -0400

    Fix typo

--- src/sh/configure-aws.sh
+++ src/sh/configure-aws.sh
@@ -23,7 +23,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 apt-cache policy docker-ce
-sudo apt-get install -y docker-ceq
+sudo apt-get install -y docker-ce
 
 # install APT's pip
 sudo apt install python-pip
