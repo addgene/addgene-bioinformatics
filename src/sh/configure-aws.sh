b58b8f0b7dc9ea67f68757fff041087f98fe5f87
Raymond LeClair <raymond.leclair@addgene.org>
2019-10-31 11:43:49 -0400

    Removed apt-get cleanup, which seems necessary only for Dockerfiles.

--- src/sh/configure-aws.sh
+++ src/sh/configure-aws.sh
@@ -15,10 +15,7 @@ sudo apt-get clean all && \
         build-essential \
         pkg-config      \
         bzip2           \
-        ca-certificates && \
-    sudo apt-get clean && \
-    sudo apt-get purge && \
-    sudo rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+        ca-certificates
 
 # install Docker (from https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04)
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
