3fa2209bfd53d80cad0a224badb872b431e40f3b
Raymond LeClair <raymond.leclair@addgene.org>
2019-12-18 21:54:58 -0500

    Added tests for additional assemblers.

--- src/python/JobsTest.py
+++ src/python/JobsTest.py
@@ -7,11 +7,14 @@ from toil.job import Job
 from toil.common import Toil
 
 from ApcJob import ApcJob
+from MasurcaJob import MasurcaJob
+from NovoplastyJob import NovoplastyJob
+from PlateAssemblyJob import PlateAssemblyJob
 from ShovillJob import ShovillJob
 from SpadesJob import SpadesJob
+from SkesaJob import SkesaJob
+from UnicyclerJob import UnicyclerJob
 from WellAssemblyJob import WellAssemblyJob
-from PlateAssemblyJob import PlateAssemblyJob
-from NovoplastyJob import NovoplastyJob
 
 import utilities
 
@@ -50,9 +53,20 @@ class ToilTestCase(unittest.TestCase):
         if not os.path.exists(self.test_directory_bg):
             os.mkdir(self.test_directory_bg)
 
-        self.novoplasty_fasta = "Circularized_assembly_1_Toil.fasta"
-        self.shovill_fasta = "contigs.fa"
-        self.spades_fasta = "contigs.fasta"
+        self.test_masurca_fasta = "final.genome.scf.fasta"
+        self.test_novoplasty_fasta = "Circularized_assembly_1_Toil.fasta"
+        self.test_shovill_fasta = "contigs.fa"
+        self.test_skesa_fasta = "contigs.fa"
+        self.test_spades_fasta = "contigs.fasta"
+        self.test_unicycler_fasta = "assembly.fasta"
+
+        self.actual_masurca_fasta = "assembly_masurca.fasta"
+        self.actual_novoplasty_fasta = "assembly_novoplasty.fasta"
+        self.actual_shovill_fasta = "assembly_shovill.fasta"
+        self.actual_skesa_fasta = "assembly_skesa.fasta"
+        self.actual_spades_fasta = "assembly_spades.fasta"
+        self.actual_unicycler_fasta = "assembly_unicycler.fasta"
+
         self.apc_fasta = "apc.1.fa"
 
     def tearDown(self):
@@ -73,17 +87,42 @@ class ToilTestCase(unittest.TestCase):
             toil, os.path.abspath(self.output_directory_ab))
         return contigs_file_id
 
-    def _assert_true_cmp_fasta(self, test_directory, actual_directory, fasta):
-        with open(os.path.join(test_directory, fasta), 'r') as f:
+    def _assert_true_cmp_fasta(self,
+                               test_directory, test_fasta,
+                               actual_directory, actual_fasta):
+        with open(os.path.join(test_directory, test_fasta), 'r') as f:
             test_lines = f.readlines()
-        with open(os.path.join(actual_directory, fasta), 'r') as f:
+        with open(os.path.join(actual_directory, actual_fasta), 'r') as f:
             actual_lines = f.readlines()
         self.assertTrue(test_lines[1:] == actual_lines[1:])
 
 
 class JobsTestCase(ToilTestCase):
 
+    def test_masurca_job(self):
+
+        options = Job.Runner.getDefaultOptions("masurcaFileStore")
+
+        with Toil(options) as toil:
+
+            read_one_file_ids, read_two_file_ids = self._import_read_files(
+                toil, [self.well_spec_b])
+
+            masurca_job = MasurcaJob(
+                read_one_file_ids[0],
+                read_two_file_ids[0],
+            )
+            masurca_rv = toil.start(masurca_job)
+
+            utilities.exportFiles(
+                toil, self.test_directory_ab, masurca_rv['masurca_rv'])
+
+        self._assert_true_cmp_fasta(
+            self.test_directory_ab, self.test_masurca_fasta,
+            self.actual_directory_ab, self.actual_masurca_fasta)
+
     def test_novoplasty_job(self):
+
         options = Job.Runner.getDefaultOptions("novoplastyFileStore")
 
         with Toil(options) as toil:
@@ -101,8 +140,8 @@ class JobsTestCase(ToilTestCase):
                 toil, self.test_directory_aa, novoplasty_rv['novoplasty_rv'])
 
         self._assert_true_cmp_fasta(
-            self.test_directory_aa, self.actual_directory_aa,
-            self.novoplasty_fasta)
+            self.test_directory_aa, self.test_novoplasty_fasta,
+            self.actual_directory_aa, self.actual_novoplasty_fasta)
 
     def test_shovill_job(self):
 
@@ -124,8 +163,30 @@ class JobsTestCase(ToilTestCase):
                 toil, self.test_directory_ab, shovill_rv['shovill_rv'])
 
         self._assert_true_cmp_fasta(
-            self.test_directory_ab, self.actual_directory_ab,
-            self.shovill_fasta)
+            self.test_directory_ab, self.test_shovill_fasta,
+            self.actual_directory_ab, self.actual_shovill_fasta)
+
+    def test_skesa_job(self):
+
+        options = Job.Runner.getDefaultOptions("skesaFileStore")
+
+        with Toil(options) as toil:
+
+            read_one_file_ids, read_two_file_ids = self._import_read_files(
+                toil, [self.well_spec_b])
+
+            skesa_job = SkesaJob(
+                read_one_file_ids[0],
+                read_two_file_ids[0],
+            )
+            skesa_rv = toil.start(skesa_job)
+
+            utilities.exportFiles(
+                toil, self.test_directory_ab, skesa_rv['skesa_rv'])
+
+        self._assert_true_cmp_fasta(
+            self.test_directory_ab, self.test_skesa_fasta,
+            self.actual_directory_ab, self.actual_skesa_fasta)
 
     def test_spades_job(self):
 
@@ -148,8 +209,31 @@ class JobsTestCase(ToilTestCase):
                 toil, self.test_directory_ab, spades_rv['spades_rv'])
 
         self._assert_true_cmp_fasta(
-            self.test_directory_ab, self.actual_directory_ab,
-            self.spades_fasta)
+            self.test_directory_ab, self.test_spades_fasta,
+            self.actual_directory_ab, self.actual_spades_fasta)
+
+    def test_unicycler_job(self):
+
+        options = Job.Runner.getDefaultOptions("unicyclerFileStore")
+
+        with Toil(options) as toil:
+
+            read_one_file_ids, read_two_file_ids = self._import_read_files(
+                toil, [self.well_spec_b])
+
+            unicycler_job = UnicyclerJob(
+                read_one_file_ids[0],
+                read_two_file_ids[0],
+                self.output_directory_ab,
+            )
+            unicycler_rv = toil.start(unicycler_job)
+
+            utilities.exportFiles(
+                toil, self.test_directory_ab, unicycler_rv['unicycler_rv'])
+
+        self._assert_true_cmp_fasta(
+            self.test_directory_ab, self.test_unicycler_fasta,
+            self.actual_directory_ab, self.actual_unicycler_fasta)
 
     def test_apc_job(self):
 
@@ -245,9 +329,12 @@ class PlateAssemblyJobTestCase(ToilTestCase):
 if __name__ == '__main__':
 
     jobsTestSuite = unittest.TestSuite()
+    jobsTestSuite.addTest(JobsTestCase("test_masurca_job"))
     jobsTestSuite.addTest(JobsTestCase("test_novoplasty_job"))
     jobsTestSuite.addTest(JobsTestCase("test_shovill_job"))
+    jobsTestSuite.addTest(JobsTestCase("test_skesa_job"))
     jobsTestSuite.addTest(JobsTestCase("test_spades_job"))
+    jobsTestSuite.addTest(JobsTestCase("test_unicycler_job"))
     jobsTestSuite.addTest(JobsTestCase("test_apc_job"))
 
     wellAssemblyJobTestSuite = unittest.TestLoader().loadTestsFromTestCase(
