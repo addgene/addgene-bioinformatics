1b9f2495e739167c02986c8ac4b954c38c465795
Raymond LeClair <raymond.leclair@springbok.io>
2022-05-11 16:24:27 -0400

    Add work in progress

--- src/python/algorithm/scratch.py
+++ src/python/algorithm/scratch.py
@@ -23,9 +23,13 @@ RPS_SEQUENCES_FILE = (
 qcs_data = pd.read_csv(SEQUENCING_BASE_DIR / QCS_SEQUENCES_FILE)
 rps_data = pd.read_csv(SEQUENCING_BASE_DIR / RPS_SEQUENCES_FILE)
 
-unq_rps_data = rps_data[~rps_data.duplicated(["Plasmid ID", "Sequence ID"])].reset_index()
+unq_rps_data = rps_data[
+    ~rps_data.duplicated(["Plasmid ID", "Sequence ID"])
+].reset_index()
 unq_qcs_data = qcs_data.merge(unq_rps_data, on=["Plasmid ID", "Sequence ID"])
-unq_qcs_data = unq_qcs_data[~unq_qcs_data.duplicated(["Plasmid ID", "Sequence ID"])].reset_index()
+unq_qcs_data = unq_qcs_data[
+    ~unq_qcs_data.duplicated(["Plasmid ID", "Sequence ID"])
+].reset_index()
 
 plate = unq_qcs_data.Plate[0]
 well = unq_qcs_data.Well[0]
@@ -37,7 +41,26 @@ working_dir = Path(os.getcwd()) / "assembly" / plate / (well + "/")
 
 os.makedirs(working_dir, exist_ok=True)
 
-adapter_fnm = au.copy_adapters(str(SEQUENCING_DATA_DIR), working_dir) 
+adapter_fnm = au.copy_adapters(str(SEQUENCING_BASE_DIR), working_dir)
 read_fnms = au.copy_reads(str(SEQUENCING_DATA_DIR), plate, well, working_dir)
+(
+    spades_seq,
+    apc_seq,
+    rd1_unmerged_fnm,
+    rd2_unmerged_fnm,
+    rds_merged_fnm,
+) = au.assemble_using_spades(
+    working_dir, read_fnms[0], read_fnms[1], preprocess=True, force=True
+)
 
-au.assemble_using_spades(working_dir, read_fnms[0], read_fnms[1], preprocess=True, force=True)
+coverage, out_fnm, covstats_fnm = au.compute_coverage_using_bbtools(
+    working_dir, rd1_unmerged_fnm, rd2_unmerged_fnm, "contigs.fasta", force=True
+)
+
+"""
+with au.pushd(working_dir):
+    k_mers_rd1, seq_rcds_rd1 = au.count_k_mers_in_rds(read_fnms[0])
+    k_mers_rd2, seq_rcds_rd2 = au.count_k_mers_in_rds(read_fnms[1])
+    k_mers_urd1, seq_rcds_urd1 = au.count_k_mers_in_rds(rd1_unmerged_fnm)
+    k_mers_urd2, seq_rcds_urd2 = au.count_k_mers_in_rds(rd2_unmerged_fnm)
+"""
